6b9b0a20f9a9adc4ef8b13248837580165cd1115
MrTrnr <marietournier.pro@gmail.com>
2020-09-17 20:58:40 +0200

    bugfix de l'autoslider qui saute avec le défilement manuel, ajout balise meta UTF 8

--- animation.js
+++ animation.js
@@ -1,26 +1,40 @@
 $(document).ready(function(){
     
-var $carrousel = $('#carrousel'), // on cible le bloc du carrousel
+let $carrousel = $('#carrousel'), // on cible le bloc du carrousel
     $img = $('#carrousel img'), // on cible les images contenues dans le carrousel
     indexImg = $img.length - 1, // on définit l'index du dernier élément
-    i = 0, // on initialise un compteur
-    $currentImg = $img.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+    i = 0; // on initialise un compteur image
+    
+function printImg(){ //fonction d'affichage des images
+    $img.css('display', 'none'); // on cache les images
+    $currentImg = $img.eq(i); // on définit la nouvelle image
+    $currentImg.css('display', 'block'); // puis on l'affiche
+}
 
-$img.css('display', 'none'); // on cache les images
-$currentImg.css('display', 'block'); // on affiche seulement l'image courante
+var intervalId = null; //déclaration de ma variable intervalId contenant la fonction setInterval, sera stoppée via la fonction clearInterval
 
-$('.next').click(function(){ // image suivante
-    if(i < indexImg){ // si le compteur est inférieur au dernier index
+function finish(){ //définition de la fonction finish
+    clearInterval(intervalId); //fonction qui va stopper intervalId
+}
+
+function newImage(){ //fonction de remplacer l'ancienne image par une nouvelle image
+      
+        if(i < indexImg){ // si le compteur est inférieur au dernier index
         i++; // on l'incrémente
         
-    }
-    else{ // sinon, on le remet à 0 (première image), permet le défilement infini
-        i = 0;
-    }
-        $img.css('display', 'none'); // on cache les images
-        $currentImg = $img.eq(i); // on définit la nouvelle image
-        $currentImg.css('display', 'block'); // puis on l'affiche
-});
+        }
+        else{ // sinon, on le remet à 0 (première image), permet le défilement infini
+            i = 0;
+        }
+        printImg();//affiche image
+}
+
+function start(){ //fonction lançant l'autoslider
+  intervalId = setInterval(newImage, 2000); //attend 2 secondes avant de lancer la fonction newImage puis se répète indéfiniment
+}
+
+
+printImg();//on affiche la premiere image
 
 $('.prev').click(function(){ // image précédente
     if(i > 0){ // si le compteur est supérieur à l'index de départ
@@ -30,38 +44,26 @@ $('.prev').click(function(){ // image précédente
         else{ // sinon, on le remet à index maximum (dernière image), permet le défilement infini
             i = indexImg;
         }
-            $img.css('display', 'none'); // on cache les images
-            $currentImg = $img.eq(i); // on définit la nouvelle image
-            $currentImg.css('display', 'block'); // puis on l'affiche
-
-
-
-
-
+            printImg();//affiche image
+            finish(); //stop l'autoslider
+            start(); //relance l'autoslider en remettant à zéro le délai, sans ces deux lignes, l'autoslider "saute" en défilement manuel
 });
 
-function slideImg(){
-    setTimeout(function(){ // on utilise une fonction anonyme
-        // console.log('i= ' + i);
-						
-        if(i < indexImg){ // si le compteur est inférieur au dernier index
-	    i++; // on l'incrémente
+$('.next').click(function(){ // image suivante
+    if(i < indexImg){ // si le compteur est inférieur au dernier index
+        i++; // on l'incrémente
         
-	}
-	else{ // sinon, on le remet à 0 (première image)
-	    i = 0;
-	}
+        }
+        else{ // sinon, on le remet à 0 (première image), permet le défilement infini
+            i = 0;
+        }
+            printImg();//affiche image
+            finish(); //stop l'autoslider
+            start(); // relance l'autoslider en remettant à zéro le délai, sans ces deux lignes, l'autoslider "saute" en défilement manuel
+});
 
-	$img.css('display', 'none');
-
-	$currentImg = $img.eq(i);
-	$currentImg.css('display', 'block');
-
-	slideImg(); // on oublie pas de relancer la fonction à la fin
-
-    }, 2000); // on définit l'intervalle à 2000 millisecondes (2s)
-}
-
-slideImg(); // enfin, on lance la fonction une première fois
+start(); //lance une première fois l'autoslider
 
 });
+
+
